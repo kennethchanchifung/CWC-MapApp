b7c6617d20dd788c69c3a9992315662aad94dbf7
kenneth <che1428@hotmail.com>
2022-11-22 21:44:59 +0800

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by k on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
